1ffd344d5df62b0ca788d662cc084743685d9cd4
kierra <51520037+kierrakay@users.noreply.github.com>
2020-08-10 14:51:18 -0400

    Done.

---
+++ src/Messages.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+class Messages extends React.Component {
+  render() {
+    return <div>
+        <h1>This is my messages component!</h1>
+        <li>one message</li>
+        <li>two message</li>
+        <li>three message</li>
+    </div>;
+  }
+}
+ 
+export default Messages;
