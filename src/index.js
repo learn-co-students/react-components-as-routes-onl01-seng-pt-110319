1b721b7affe8856102cea130e4ed531bfd902e94
antony_sanders <antony.devon92@gmail.com>
2020-08-13 01:33:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Login from './pages/login';
+import Navbar from './navBar';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
+const navStyle = {
+  margin: '30px 0 30px 0',
 };
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <div style={navStyle}>
+      <Navbar/>
+      </div>
+      <Switch>
+        <Route path='/about' component={About} />
+        <Route path='/login' component={Login} />
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </div>
+  </Router>,
   document.getElementById('root')
 );
